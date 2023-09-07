1fa53c12f6ad7d0c19354c3f7a1eec1454706a7c
FrancescoPepi <francescopepi16@gmail.com>
2023-09-07 17:48:58 +0200

    Add structure

---
+++ vite-project/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import galleria from "./components/galleria.vue";
+</script>
+
+<template>
+  <HelloWorld></HelloWorld>
+  <galleria></galleria>
+</template>
+
+<style scoped></style>
---
+++ vite-project/src/components/galleria.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      img: "../../img/mobile-logo.png",
+    };
+  },
+
+  methods: {
+    printImg(img) {
+      const imgUrl = new URL(img, import.meta.url);
+      console.log(imgUrl);
+      return imgUrl.href;
+    },
+  },
+};
+</script>
+<template>
+  <div>
+    <img :src="printImg(img)" alt="asdas" />
+  </div>
+</template>
+<style lang="scss" scoped></style>
